9712a4fabc2ff2ff95dc3cc521359f2462fcfb22
Dan Muckerman <danielmuckerman@me.com>
2023-04-21 12:31:17 -0400

    chaoschaos map

---
+++ keyboards/chaoschaos/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x72, 0x39, 0x9F, 0x43, 0xCE, 0xE9, 0x04, 0x08}
+
+#define ENCODERS_PAD_A { B4 }
+#define ENCODERS_PAD_B { D7 }
+
+/*#define VIAL_COMBO_ENTRIES 30
+  #define VIAL_KEY_OVERRIDE_ENTRIES 4
+  #define VIAL_TAP_DANCE_ENTRIES 4*/
