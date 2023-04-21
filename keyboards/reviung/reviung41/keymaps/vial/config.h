d3b202fce2638bdef8f31e55b45c7c9e3590ab0d
Dan Muckerman <danielmuckerman@me.com>
2023-04-21 12:32:02 -0400

    reviung41 map

--- keyboards/reviung/reviung41/keymaps/vial/config.h
+++ keyboards/reviung/reviung41/keymaps/vial/config.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 
 #define VIAL_KEYBOARD_UID {0x40, 0x4E, 0xA1, 0x89, 0x23, 0x14, 0x82, 0x88}
@@ -6,3 +7,15 @@
 // top right key (row 3, column 5):
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 5 }
+=======
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x77, 0x86, 0x5D, 0x39, 0x32, 0x4C, 0xDC, 0x7D}
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
+#define RGBLIGHT_LAYERS
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
+>>>>>>> c322bc8cf5 (reviung41 map)
