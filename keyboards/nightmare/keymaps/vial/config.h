1c75c26a0dd8889a530f625005a4dcf559e737bb
Dan Muckerman <danielmuckerman@me.com>
2023-08-01 10:33:00 -0400

    nightmare vial firmware

---
+++ keyboards/nightmare/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0E, 0x3E, 0xC3, 0x10, 0xF0, 0xA6, 0xF6, 0x65}
+
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 13 }
