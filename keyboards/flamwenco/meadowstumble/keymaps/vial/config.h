b96d901a8b3fd107bd4f0b5a8d4a6f28a87b32ee
Dan Muckerman <danielmuckerman@me.com>
2023-08-01 10:31:16 -0400

    create meadowstumble firmware

---
+++ keyboards/flamwenco/meadowstumble/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x91, 0xBA, 0x8B, 0xA7, 0xB9, 0xF4, 0x11, 0xB4}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
